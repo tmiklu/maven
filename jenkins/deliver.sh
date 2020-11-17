0ed394cdbed35b71849353916be0eaeb83d87352
root <root@sighup.local>
2020-11-17 13:00:57 +0100

    Change jar to cp

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -23,4 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -cp target/${NAME}-${VERSION}.jar com.mycompany.app.App
+#java -jar target/${NAME}-${VERSION}.jar 
